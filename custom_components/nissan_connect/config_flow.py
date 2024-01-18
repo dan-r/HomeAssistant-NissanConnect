2e4fcceee424474100517bbb8a4d6916092639fd
Daniel Raper <me@danr.uk>
2024-01-18 16:33:38 +0000

    Add credential validation on config flow

--- custom_components/nissan_connect/config_flow.py
+++ custom_components/nissan_connect/config_flow.py
@@ -1,8 +1,8 @@
 import voluptuous as vol
 from homeassistant.config_entries import (ConfigFlow, OptionsFlow)
 from .const import DOMAIN, CONFIG_VERSION
+from .kamereon import NCISession
 import homeassistant.helpers.config_validation as cv
-
 
 USER_SCHEMA = vol.Schema({
     vol.Required("email"): cv.string,
@@ -26,12 +26,24 @@ class NissanConfigFlow(ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if info is not None:
-            self._abort_if_unique_id_configured()
-        
-            return self.async_create_entry(
-                title="Car",
-                data=info
+            # Validate credentials
+            kamereon_session = NCISession(
+                region=info["region"]
             )
+
+            try:
+                await self.hass.async_add_executor_job(kamereon_session.login,
+                    info["email"],
+                    info["password"]
+                )
+            except:
+                errors["base"] = "auth_error"
+            
+            if len(errors) == 0:
+                return self.async_create_entry(
+                    title="NissanConnect Account",
+                    data=info
+                )
 
         return self.async_show_form(
             step_id="user", data_schema=USER_SCHEMA, errors=errors
